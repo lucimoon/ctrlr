f0d363889d513f4074a295c00ce167d8ac7616b0
Luci Moon <lucilla.moonilla@gmail.com>
2018-01-06 21:01:44 -0800

    Add Optional Health Slider

--- Damagable/Health.cs
+++ Damagable/Health.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace tardigrage_alpha.Assets.Scripts
 {
@@ -6,15 +7,29 @@ namespace tardigrage_alpha.Assets.Scripts
     {
         [SerializeField]
         private float health = 100f;
+        [SerializeField]
+        private Slider healthSlider;
+
+        void Start() {
+          UpdateUI();
+        }
 
         public void Reduce(float damage) {
           health -= Mathf.Abs(damage);;
           Debug.Log("health: " + health);
           if (health <= 0f) Kill();
+
+          UpdateUI();
         }
 
         private void Kill () {
             Destroy(gameObject);
+        }
+
+        private void UpdateUI () {
+          if (healthSlider != null) {
+            healthSlider.value = health;
+          }
         }
     }
 }
