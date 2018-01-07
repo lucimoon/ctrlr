6de23e917f26c475b9c20dd03e7c7a84f7d038e9
Luci Moon <lucilla.moonilla@gmail.com>
2018-01-06 17:30:05 -0800

    togglable keep character within bounds

--- TopDown/TopDownController.cs
+++ TopDown/TopDownController.cs
@@ -9,8 +9,15 @@ namespace tardigrage_alpha.Assets.Scripts.Ctrlr
   [RequireComponent(typeof(TDRight))]
 	public class TopDownController : MonoBehaviour
 	{
+    [SerializeField]
+    private Camera mainCamera;
+
 		[SerializeField]
 		private float speed = 5f;
+
+    [SerializeField]
+		private bool keepWithinBounds = false;
+
 		private Dictionary<Direction, Vector3> directionMap = new Dictionary<Direction, Vector3>
 		{
 			{Direction.up, Vector3.up},
@@ -20,10 +27,40 @@ namespace tardigrage_alpha.Assets.Scripts.Ctrlr
 		};
 
 		public void Move (Direction direction) {
-			if (directionMap.ContainsKey(direction)) {
+      bool withinBounds = GetWithinBounds(direction);
+
+			if (directionMap.ContainsKey(direction) && withinBounds) {
 				Vector3 vector = directionMap[direction] * speed * Time.deltaTime;
 				transform.Translate(vector, Space.World);
 			}
 		}
+
+    private bool GetWithinBounds(Direction direction) {
+      bool withinBounds = true;
+
+      if (!keepWithinBounds) return withinBounds;
+
+      Vector3 viewportPosition = mainCamera.WorldToViewportPoint(gameObject.transform.position);
+
+      switch (direction)
+      {
+        case Direction.left:
+          withinBounds = viewportPosition.x > 0;
+          break;
+        case Direction.right:
+          withinBounds = viewportPosition.x < 1;
+          break;
+        case Direction.up:
+          withinBounds = viewportPosition.y < 1;
+          break;
+        case Direction.down:
+          withinBounds = viewportPosition.y > 0;
+          break;
+        default:
+          break;
+      }
+
+      return withinBounds;
+    }
 	}
 }
