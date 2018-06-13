41ddace977bce8645ec6e94163b5d93538c60ac7
Luci Moon <lucilla.moonilla@gmail.com>
2018-06-12 19:43:42 -0700

    stop/start time when UI toggled appropriately

--- UI/UIController.cs
+++ UI/UIController.cs
@@ -9,15 +9,20 @@ public class UIController : MonoBehaviour {
   }
 
   public void ToggleUI () {
-    canvas.enabled = !canvas.enabled;
+    if (canvas.enabled) {
+      DisableUI();
+    } else {
+      EnableUI();
+    }
   }
 
   public void EnableUI () {
     canvas.enabled = true;
+    Time.timeScale = 0f;
   }
 
   public void DisableUI () {
     canvas.enabled = false;
+    Time.timeScale = 1f;
   }
-
 }
